b04d32ca212513a9eeec22743285727b3600d3af
vlts1 <vladyslavtsepenok@gmail.com>
2023-01-12 11:22:47 -0800

    Average calculator

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # HW2
+P1 Vital Kyungu
+P2 Vladyslav Tsepenok
--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-document.addEventListener("DOMContentLoaded", function (event) {
+function calculateAverage() {
+    let val1 = parseInt(document.getElementById("value1").value); 
+    let val2 = parseInt(document.getElementById("value2").value); 
+    let val3 = parseInt(document.getElementById("value3").value); 
 
-    document.getElementById("button1").addEventListener("click", function () {
-
-        document.getElementById("textbox1").value = "success";
-    });
-});
-
+    let averageValue = (val1 + val2 + val3) / 3;
+    document.getElementById("result").innerHTML = averageValue;
+}
