bf6a464b993e287ce8f67d6ef957baebaf0c011e
vlts1 <vladyslavtsepenok@gmail.com>
2023-01-20 22:31:03 -0800

    Three buttons calc

--- main.js
+++ main.js
@@ -1,8 +1,39 @@
-function calculateAverage() {
-    let val1 = parseInt(document.getElementById("value1").value); 
-    let val2 = parseInt(document.getElementById("value2").value); 
-    let val3 = parseInt(document.getElementById("value3").value); 
+const origArray = [2,3,4];
 
-    let averageValue = (val1 + val2 + val3) / 3;
-    document.getElementById("result").innerHTML = averageValue;
+const f1 = document.getElementById('field1')
+const f2 = document.getElementById('field2')
+const f3 = document.getElementById('field3')
+
+window.addEventListener('DOMContentLoaded', (event) => {
+    f1.value = origArray[0];
+    f2.value = origArray[1];
+    f3.value = origArray[2];
+});
+
+function squareValues() {
+    var newValues = origArray.map(function(value){
+        return Math.pow(value, 2);
+    });
+    f1.value = newValues[0];
+    f2.value = newValues[1];
+    f3.value = newValues[2];
+}
+document.getElementById('square').addEventListener("click", squareValues);
+
+document.getElementById('cube').addEventListener("click", function() {
+    var newValues = origArray.map(function(value){
+        return Math.pow(value, 3);
+    });
+    f1.value = newValues[0];
+    f2.value = newValues[1];
+    f3.value = newValues[2];
+});
+
+function toTheFourthValues() {
+    var newValues = origArray.map(function(value){
+        return Math.pow(value, 4);
+    });
+    f1.value = newValues[0];
+    f2.value = newValues[1];
+    f3.value = newValues[2];
 }
